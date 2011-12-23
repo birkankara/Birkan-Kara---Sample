d122e8c5aff2f54af2ee93e8206282f7c643e190
Birkan Kara <mobilmedia@hotmail.com>
2011-12-23 19:58:51 +0200

    Birkan Kara mobilmedia@hotmail.com info@medyadev.com 0535 6905775

---
+++ MyProductList/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyProductList.Models;
+
+namespace MyProductList.Controllers
+{ 
+    public class HomeController : Controller
+    {
+        private ProductContext db = new ProductContext();
+
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            //return View(db.Products.ToList());
+            using (var db = new ProductContext())
+            {
+                return View(db.Products.ToList());//does it
+            }
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyProductList/Models/ProductContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MyProductList.Models
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class MyDatabaseInitializer :
+        DropCreateDatabaseIfModelChanges<ProductContext>
+    //CreateDatabaseIfNotExists<ProductContext>
+    {
+        protected override void Seed(ProductContext context)
+        {
+            var products = new List<Product>{
+            new Product { Name = "Prod 01" },
+            new Product { Name = "Prod 02" },
+            new Product { Name = "Prod 03" },
+            new Product { Name = "Prod 04" },
+            new Product { Name = "Prod 05" },
+            new Product { Name = "Prod 06" },
+            new Product { Name = "Prod 07" },
+            new Product { Name = "Prod 08" },
+            new Product { Name = "Prod 09" },
+            new Product { Name = "Prod 10" }
+            };
+
+            products.ForEach(Product => context.Products.Add(Product));
+            context.SaveChanges();
+
+
+        }
+    }
+
+}
